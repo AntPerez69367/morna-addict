79489948b3cd8a6f05e1489991145cdbc08f3ff7
Anthony Perez <apere737@fiu.edu>
2020-05-17 22:54:52 -0500

    database scripts

---
+++ data/dailyReset.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+db_file = r'./mornatk.db'
+connection = None
+try:
+    connection = sqlite3.connect(db_file)
+except Exception as e:
+    print(e)
+
+if connection is not None:
+    sql = ''' UPDATE players SET daily = 0'''
+    cur = connection.cursor()
+    cur.execute(sql)
+    connection.commit()
+    connection.close
