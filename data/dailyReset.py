21691787d07afbefe395357cea7db01f3f13dc9d
Anthony Perez <anthonyj.perez@gmail.com>
2020-05-20 08:14:08 -0500

    fix dailyReset logging

--- data/dailyReset.py
+++ data/dailyReset.py
@@ -5,18 +5,18 @@ def currentTime():
     return time.strftime('%m-%d-%Y_%H%M', time.localtime())
 
 
-print("[%s] Starting Daily Reset".format(currentTime()))
+print("[{0}] Starting Daily Reset".format(currentTime()))
 db_file = r'./mornatk.db'
 connection = None
 try:
-    print("[%s] Connecting to Database".format(currentTime()))
+    print("[{0}] Connecting to Database".format(currentTime()))
     connection = sqlite3.connect(db_file)
 except Exception as e:
-    print("[%s] Error Connecting to Database".format(currentTime()))
+    print("[{0}] Error Connecting to Database".format(currentTime()))
     print(e)
 
 if connection is not None:
-    print("[%s] Resetting DailyXP".format(currentTime()))
+    print("[{0}] Resetting DailyXP".format(currentTime()))
     sql = ''' UPDATE players SET daily = 0'''
     cur = connection.cursor()
     cur.execute(sql)
@@ -24,4 +24,4 @@ if connection is not None:
     connection.close
 
 
-print("[%s] Daily Reset Complete".format(%currentTime()))
+print("[{0}] Daily Reset Complete".format(currentTime()))
