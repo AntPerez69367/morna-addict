ec6b434f40c8a3d150c13c2b862d1d042b1be90f
Anthony Perez <apere737@fiu.edu>
2020-05-22 21:08:25 -0500

    fix header still

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,25 +5,34 @@ import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined"
 import ButtonBase from "@material-ui/core/ButtonBase"
 import Typography from "@material-ui/core/Typography"
 import calculator from "../../images/icons/calculator.svg"
-import {Link} from '@reach/router'
+import { Link } from "@reach/router"
 
 const useStyles = makeStyles({
+  topMenu: {
+    height: "150px",
+    width: "100%",
+    position: "absolute",
+    top: "0",
+    left: "0",
+    backgroundColor: "#1F2833",
+    activeColor: "#f7f7f7",
+  },
   logo: {
-    width: '250px',
-    height: '125px',
+    width: "250px",
+    height: "125px",
   },
   logoContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '10px'
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "10px",
   },
   navigationBar: {
     height: "30px",
     width: "100%",
     zIndex: "2",
     position: "fixed",
-    top: "130",
+    top: "250",
     left: "0",
     backgroundColor: "#0B0C10",
     activeColor: "#f7f7f7",
@@ -50,42 +59,53 @@ const Header = () => {
     {
       id: 0,
       title: "Home",
-      icon: (<HomeOutlinedIcon style={{paddingRight: '5px'}}/>),
+      icon: <HomeOutlinedIcon style={{ paddingRight: "5px" }} />,
       path: "/",
     },
     {
       id: 1,
       title: "Calculators",
-      icon: <img style={{paddingRight: '5px'}} alt="calculator icon" src={calculator} />,
+      icon: (
+        <img
+          style={{ paddingRight: "5px" }}
+          alt="calculator icon"
+          src={calculator}
+        />
+      ),
       path: "/app/calculator",
     },
   ]
   return (
-    <>
+    <div className={classes.topMenu}>
       <div className={classes.logoContainer}>
-        <Link to={'/'} >
-        <img alt="logo" className={classes.logo} src={logoSVG} />
+        <Link to={"/"}>
+          <img alt="logo" className={classes.logo} src={logoSVG} />
         </Link>
       </div>
 
       <div className={classes.navigationBar}>
         {menu.map(item => {
           return (
-            <ButtonBase key={item.id} focusRipple className={classes.headerMenu}>
+            <ButtonBase
+              key={item.id}
+              focusRipple
+              className={classes.headerMenu}
+            >
               <Link to={item.path}>
-              <Typography
-                className={classes.buttonText}
-                component="div"
-                variant="subtitle2"
-              >
-                 {item.icon}{item.title}
-              </Typography>
+                <Typography
+                  className={classes.buttonText}
+                  component="div"
+                  variant="subtitle2"
+                >
+                  {item.icon}
+                  {item.title}
+                </Typography>
               </Link>
             </ButtonBase>
           )
         })}
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,34 +1,17 @@
 import React from "react"
-import { makeStyles } from "@material-ui/core/styles"
 import { Helmet } from "react-helmet"
 import Header from "../Header"
 import View from "../View"
 
-const useStyles = makeStyles({
-  topMenu: {
-    height: "150px",
-    width: "100%",
-    zIndex: "1",
-    position: "fixed",
-    top: "0",
-    left: "0",
-    backgroundColor: "#1F2833",
-    activeColor: "#f7f7f7",
-  },
-})
-
 const Layout = ({ children }) => {
-  const classes = useStyles()
   return (
-    <div>
+    <>
       <Helmet title="MornaAddict" />
-      <div className={classes.topMenu}>
       <Header />
-      </div>
       <View>
       <main>{children}</main>
       </View>
-    </div>
+    </>
   )
 }
 
