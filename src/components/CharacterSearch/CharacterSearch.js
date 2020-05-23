df9a9ff19bd3e18a2786500371d7d72816fea459
Anthony Perez <apere737@fiu.edu>
2020-05-22 19:34:08 -0500

    Fix searchbox

--- src/components/CharacterSearch/CharacterSearch.js
+++ src/components/CharacterSearch/CharacterSearch.js
@@ -5,13 +5,11 @@ import { makeStyles } from "@material-ui/styles"
 import { InputBase, IconButton } from "@material-ui/core"
 import SearchIcon from "@material-ui/icons/Search"
 import CharacterDetails from "./CharacterDetails"
-import Table from "@material-ui/core/Table"
 
 const useStyles = makeStyles({
   root: {
     padding: "2px 4px",
     display: "flex",
-    alignItems: "center",
     width: "99%",
   },
   header: {
@@ -24,7 +22,7 @@ const useStyles = makeStyles({
   },
   input: {
     marginLeft: "theme.spacing(1)",
-    width: '93%'
+    width: "93%",
   },
   iconButton: {
     padding: "10px",
@@ -70,27 +68,31 @@ const CharacterSearch = () => {
   }
 
   return (
-    <Paper component="form" onSubmit={handleSubmit} className={classes.root}>
-        <Table>
-          <InputBase
-            className={classes.input}
-            id="search-query"
-            name="char"
-            onChange={handleChange}
-            placeholder="Character Search"
-            inputProps={{ root: {marginLeft: '15px'}, "aria-label": "Character Search" }}
-          />
-          <IconButton
-            type="submit"
-            className={classes.iconButton}
-            aria-label="search"
-          >
-            <SearchIcon />
-          </IconButton>
-
-          {charData && <CharacterDetails open={true} character={charData} />}
-        </Table>
-    </Paper>
+    <>
+      <Paper component="form" square onSubmit={handleSubmit} className={classes.root}>
+        <InputBase
+          className={classes.input}
+          id="search-query"
+          name="char"
+          onChange={handleChange}
+          placeholder="Character Search"
+          inputProps={{
+            root: { marginLeft: "15px" },
+            "aria-label": "Character Search",
+          }}
+        />
+        <IconButton
+          type="submit"
+          className={classes.iconButton}
+          aria-label="search"
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      <Paper style={{paddingRight: '15px'}} square>
+        {charData && <CharacterDetails open={true} character={charData} />}
+      </Paper>
+    </>
   )
 }
 
