6d1603d1d51b6adc21a404a0f0ae3b99e385d356
Anthony Perez <apere737@fiu.edu>
2020-06-03 16:19:30 -0500

    nginx config test

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,5 +75,12 @@ module.exports = {
       resolve: `gatsby-plugin-create-client-paths`,
       options: { prefixes: [`/app/*`] },
     },
+    {
+      resolve: "gatsby-plugin-nginx-redirect",
+      options: {
+        inputConfigFile: `${__dirname}/nginx.conf`,
+        outputConfigFile: `${__dirname}/nginx.out.conf`,
+      },
+    },
   ],
 }
